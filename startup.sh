bd754da99908d6df247c52653d9e42de4f1e9705
lcy2 <lichangyi888@hotmail.com>
2017-04-21 09:22:35 +0000

    Pip/Django installation added.

---
+++ startup.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# Turn on syntax coloring in vi
+
+echo -n "1. "
+if [ -f $HOME/.vimrc ]
+then
+    echo ".vimrc file already exists."
+else
+    echo "syntax on" >> $HOME/.vimrc
+    echo "set expandtab shiftwidth=4 softtabstop=0 autoindent tabstop=4" >> $HOME/.vimrc
+    echo ".vimrc file created."
+fi
+
+# Edit local git config for syncing with Github
+echo -n "2. "
+git config --global user.name "lcy2"
+git config --global user.email "lichangyi888@hotmail.com"
+echo "git config set."
+
+# turn on sound for Kali
+echo -n "3. "
+systemctl --user enable pulseaudio && systemctl --user start pulseaudio
+echo "sound turned on"
+
+# download Heroku
+echo -n "4. "
+wget https://cli-assets.heroku.com/branches/stable/heroku-linux-amd64.tar.gz -O heroku.tar.gz
+tar -xvzf heroku.tar.gz -C /usr/local/lib
+/usr/local/lib/heroku/install
+echo "installed Heroku command line utility."
+
+echo -n "5. "
+pip install --update pip
+pip install django
+echo "installed Django."
